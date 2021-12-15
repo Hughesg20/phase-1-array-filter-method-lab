4f4e4f9ccc7118b07c4482c50e9cc8250f031427
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-12-15 13:31:00 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution here
+// const drivers = [
+// 	{
+// 		name: 'Bobby',
+// 		hometown: 'Pittsburgh'
+// 	},
+// 	{
+// 		name: 'Sammy',
+// 		hometown: 'New York'
+// 	},
+// 	{
+// 		name: 'Sally',
+// 		hometown: 'Cleveland'
+// 	},
+// 	{
+// 		name: 'Annette',
+// 		hometown: 'Los Angeles'
+// 	},
+// 	{
+// 		name: 'Bobby',
+// 		hometown: 'Tampa Bay'
+// 	}
+// ];
+
+// expect(matchName(drivers, 'Bobby')).to.eql([
+// 	{
+// 		name: 'Bobby',
+// 		hometown: 'Pittsburgh'
+// 	},
+// 	{
+// 		name: 'Bobby',
+// 		hometown: 'Tampa Bay'
+// 	}
+// ]);
+
+
+
+function findMatching (drivers, string) {
+	return drivers.filter((num) => num.toLowerCase() === string.toLowerCase())
+};
+
+function fuzzyMatch(drivers, string) {
+	return drivers.filter((num) => num[0] === string[0])
+};
+
+function matchName(namesArray, string) {
+	return drivers.filter((num) => num['name'] === string)
+};
+
